d367346c9ec64613ea0943f47bc1969ba4f912f7
MintMissy <dawidkostka480@gmail.com>
2022-01-27 12:29:47 +0100

    Implement theme toggling; Add smooth theme change styles; Add theme initialization;

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+var themeToggler = document.getElementById('theme-toggler');
+var themeTogglerControllers = document.getElementById('theme-toggler-controllers').children;
+var _loop_1 = function (controller) {
+    var theme = controller.getAttribute('theme');
+    controller.addEventListener('click', function () {
+        setTheme(theme);
+        controller.classList.add('active');
+    });
+};
+// Assign on controller clicks
+for (var _i = 0, themeTogglerControllers_1 = themeTogglerControllers; _i < themeTogglerControllers_1.length; _i++) {
+    var controller = themeTogglerControllers_1[_i];
+    _loop_1(controller);
+}
+function initializeTheme() {
+    var controller = themeTogglerControllers[0];
+    var theme = controller.getAttribute('theme');
+    setTheme(theme);
+    document.body.setAttribute('currentTheme', theme);
+    controller.classList.add('active');
+}
+function setTheme(newTheme) {
+    // Toggle theme class and attribute on body
+    var currentTheme = document.body.getAttribute('currentTheme');
+    if (currentTheme === undefined) {
+        document.body.classList.toggle(currentTheme);
+    }
+    // Add new theme class and attribute to body
+    document.body.setAttribute('currentTheme', newTheme);
+    document.body.classList.add(newTheme);
+    // Remove active class from previous controller
+    for (var _i = 0, themeTogglerControllers_2 = themeTogglerControllers; _i < themeTogglerControllers_2.length; _i++) {
+        var controller = themeTogglerControllers_2[_i];
+        if (controller.classList.contains('active')) {
+            controller.classList.remove('active');
+            break;
+        }
+    }
+}
+initializeTheme();
---
+++ scripts/main.ts
@@ -0,0 +1,43 @@
+const themeToggler = document.getElementById('theme-toggler');
+const themeTogglerControllers = document.getElementById(
+  'theme-toggler-controllers'
+).children;
+
+// Assign on controller clicks
+for (const controller of themeTogglerControllers) {
+  const theme = controller.getAttribute('theme');
+  controller.addEventListener('click', () => {
+    setTheme(theme);
+    controller.classList.add('active');
+  });
+}
+
+function initializeTheme() {
+  const controller = themeTogglerControllers[0];
+  const theme = controller.getAttribute('theme');
+  setTheme(theme);
+  document.body.setAttribute('currentTheme', theme);
+  controller.classList.add('active');
+}
+
+function setTheme(newTheme: string): void {
+  // Toggle theme class and attribute on body
+  const currentTheme = document.body.getAttribute('currentTheme');
+  if (currentTheme === undefined) {
+    document.body.classList.toggle(currentTheme);
+  }
+
+  // Add new theme class and attribute to body
+  document.body.setAttribute('currentTheme', newTheme);
+  document.body.classList.add(newTheme);
+
+  // Remove active class from previous controller
+  for (const controller of themeTogglerControllers) {
+    if (controller.classList.contains('active')) {
+      controller.classList.remove('active');
+      break;
+    }
+  }
+}
+
+initializeTheme();
